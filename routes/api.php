355b29f5332de4cdcfa34290c73867518a5a456c
Nouval Rizky <nouvalr@gmail.com>
2022-10-06 18:19:33 +0800

    Create customer login

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\CustomerAuthController;
 use App\Http\Controllers\Auth\CustomerRegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -20,3 +21,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::post('customer/register', [CustomerRegisterController::class, 'create']);
+Route::post('customer/login', [CustomerAuthController::class, 'login']);
