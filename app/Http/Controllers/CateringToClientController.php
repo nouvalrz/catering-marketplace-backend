9153ed90bc67befddaaf45974667d81ec420a38e
Nouval Rizky <nouvalrz@Nouvals-MacBook-Air.local>
2022-12-09 11:08:16 +0800

    Modify relevant catering response

--- app/Http/Controllers/CateringToClientController.php
+++ app/Http/Controllers/CateringToClientController.php
@@ -20,16 +20,16 @@ class CateringToClientController extends Controller
 
         foreach ($caterings as $catering){
             $c = array();
-            $c['catering'] = DB::table('caterings')->select(['caterings.id','caterings.name','villages.name','images.original_path'])->join('villages', 'caterings.village_id', '=', 'villages.id')->join('images', 'caterings.image_id', '=', 'images.id')->where('caterings.id', $catering->id)->get()->first();
+            $c['catering'] = DB::table('caterings')->select(['caterings.id','caterings.name AS name','villages.name AS village_name','images.original_path'])->join('villages', 'caterings.village_id', '=', 'villages.id')->join('images', 'caterings.image_id', '=', 'images.id')->where('caterings.id', $catering->id)->get()->first();
 
 
             $c['products'] = DB::table('products')->select(['products.id', 'products.name', 'products.price', 'images.original_path'])->join('caterings', 'caterings.id', '=', 'products.catering_id')->join('images', 'images.id', '=', 'products.image_id')->where('products.catering_id', $catering->id)->limit(2)->get();
 
             $complete_caterings[] = $c;
-
+            $finish_caterings = array('caterings'=>$complete_caterings);
         }
-
-        return json_encode($complete_caterings);
+        return response()->json($finish_caterings);
+//        return json_encode($finish_caterings);
 
     }
 }
