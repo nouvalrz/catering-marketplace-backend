873fe605724f3166ed5936dd4355aa3a4f671f8c
Nouval Rizky <nouvalr@gmail.com>
2023-06-12 21:19:22 +0800

    fix complaint model and logic

--- app/Models/Complaints.php
+++ app/Models/Complaints.php
@@ -8,23 +8,24 @@ use Illuminate\Database\Eloquent\Model;
 class Complaints extends Model
 {
     use HasFactory;
-    
+
     protected $fillable = [
-        'order_id',
+        'orders_id',
         'status',
         'problem',
+        'solution_type'
     ];
 
     public function orders()
     {
         return $this->belongsTo(Orders::class, 'orders_id');
     }
-    
+
     public function complaintImages()
     {
         return $this->hasMany(ComplaintImages::class, 'complaint_id');
     }
-    
+
     public function getImageAttribute($image)
     {
         return asset('storage/complaints/' . $image);
